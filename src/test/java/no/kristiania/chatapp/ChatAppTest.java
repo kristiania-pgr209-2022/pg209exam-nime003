a49136f7d7685c1a6e25b999af00cb637b6396e4
nime003 <nime003@student.kristiania.no>
2022-11-08 19:45:14 +0100

    🔴httpResponseShouldBe200Test created test - Does not compile

---
+++ src/test/java/no/kristiania/chatapp/ChatAppTest.java
@@ -0,0 +1,24 @@
+package no.kristiania.chatapp;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ChatAppTest {
+
+    private ChatApp server;
+
+    @Test
+    void httpResponseShouldBe200Test() throws IOException {
+        var con = openConnection("/");
+        assertThat(con.getResponseCode()).isEqualTo(200);
+    }
+
+    private HttpURLConnection openConnection(String spec) throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), spec).openConnection();
+    }
+
+}
