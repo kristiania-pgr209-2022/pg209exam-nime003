a70cc34bdb0927bf69ad1bd90286a3b94a14a0eb
nime003 <nime003@student.kristiania.no>
2022-11-09 17:40:26 +0100

    🟢httpResponseShouldBe200Test

--- src/main/java/no/kristiania/chatapp/ChatAppServer.java
+++ src/main/java/no/kristiania/chatapp/ChatAppServer.java
@@ -1,6 +1,9 @@
 package no.kristiania.chatapp;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,6 +13,12 @@ public class ChatAppServer {
 
     public ChatAppServer(int port) {
         this.ChatAppServer = new Server(port);
+
+        var wContext = new WebAppContext();
+        wContext.setContextPath("/");
+        wContext.setBaseResource(Resource.newClassPathResource("/webapp"));
+
+        ChatAppServer.setHandler(new HandlerList(wContext));
     }
     public void start() throws Exception {
         ChatAppServer.start();
