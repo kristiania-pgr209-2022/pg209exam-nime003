0336fbc81054196688c97b90df5efa6bef28245f
“Mebu98” <merijn.buitelaar98@gmail.com>
2022-11-10 17:47:53 +0100

    🔴shouldSaveAndRetrieveUser (does not even compile)

---
+++ src/test/java/no/kristiania/chatapp/db/jdbc/UserDaoTest.java
@@ -0,0 +1,31 @@
+package no.kristiania.chatapp.db.jdbc;
+
+import no.kristiania.chatapp.db.InMemoryDatasource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+    private UserDao dao;
+
+    @BeforeEach
+    void setup(){dao = new JdbcUserDao(InMemoryDatasource.createTestDataSource());}
+
+    @Test
+    void shouldSaveAndRetrieveUser() throws SQLException{
+        var user = new User();
+        user.setUsername("Nils");
+        user.setPassword("sliN");
+
+        dao.save(user);
+
+        assertThat(dao.retrieveUser(user.getId))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(user)
+                .isNotSameAs(user);
+    }
+}
