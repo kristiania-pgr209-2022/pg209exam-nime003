5c5e38859a57295e25a639ff3ca6f0b991a10733
“Mebu98” <merijn.buitelaar98@gmail.com>
2022-11-14 21:30:55 +0100

    🟢UserDaoTest

--- src/test/java/no/kristiania/chatapp/db/jdbc/UserDaoTest.java
+++ src/test/java/no/kristiania/chatapp/db/jdbc/UserDaoTest.java
@@ -3,29 +3,31 @@ package no.kristiania.chatapp.db.jdbc;
 import no.kristiania.chatapp.db.InMemoryDatasource;
 import no.kristiania.chatapp.db.User;
 import no.kristiania.chatapp.db.UserDao;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
-    private UserDao dao;
-    private final SampleData sampleData = new SampleData();
-    private final DataSource testDataSource = InMemoryDatasource.createTestDataSource();
-    @BeforeEach
-    void setup(){
-        dao = new JdbcUserDao(testDataSource);}
+    private static UserDao dao;
+    private static final SampleData sampleData = new SampleData();
+    private static final DataSource testDataSource = InMemoryDatasource.createTestDataSource();
+
+    private static ArrayList<User> users = new ArrayList<>();
+
+    @BeforeAll
+    static void setup() throws SQLException {
+        dao = new JdbcUserDao(testDataSource);
+        users = sampleData.sampleUsers(5);
+        for (var user : users) dao.save(user);
+    }
 
     @Test
     void shouldSaveAndRetrieveUser() throws SQLException{
-        var user = new User();
-        user.setUsername("Nils");
-        user.setPassword("sliN");
-
-        dao.save(user);
+        var user = users.get(1);
 
         assertThat(dao.retrieveUser(user.getId()))
                 .hasNoNullFieldsOrProperties()
@@ -36,9 +38,6 @@ public class UserDaoTest {
 
     @Test
     void shouldRetrieveAllUsers() throws SQLException {
-        var users = sampleData.sampleUsers(5);
-        for(var user : users) dao.save(user);
-
         assertThat(dao.retrieveAllUsers())
                 .usingRecursiveComparison()
                 .isEqualTo(users)
