e019a1b6a1ee9a4549170268309d06453ab2c20a
nime003 <nime003@student.kristiania.no>
2022-11-15 18:43:18 +0100

    🔵 Added Logger interface and created main method. -Refactored ChatAppServer method and fields to chatAppServer

--- src/main/java/no/kristiania/chatapp/ChatAppServer.java
+++ src/main/java/no/kristiania/chatapp/ChatAppServer.java
@@ -4,26 +4,39 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
 
 public class ChatAppServer {
-    private final Server ChatAppServer;
+    private static final Logger logger = LoggerFactory.getLogger(no.kristiania.chatapp.ChatAppServer.class);
+    private final Server chatAppServer;
 
     public ChatAppServer(int port) {
-        this.ChatAppServer = new Server(port);
+        this.chatAppServer = new Server(port);
 
         var wContext = new WebAppContext();
         wContext.setContextPath("/");
         wContext.setBaseResource(Resource.newClassPathResource("/webapp"));
 
-        ChatAppServer.setHandler(new HandlerList(wContext));
+        chatAppServer.setHandler(new HandlerList(wContext));
     }
     public void start() throws Exception {
-        ChatAppServer.start();
+        chatAppServer.start();
     }
     public URL getURL() throws MalformedURLException {
-        return ChatAppServer.getURI().toURL();
+        return chatAppServer.getURI().toURL();
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = Optional.ofNullable(System.getenv("HTTP_PLATFORM_PORT"))
+                .map(Integer::parseInt)
+                .orElse(9090);
+        var chatAppServer = new ChatAppServer(port/*, Database.getDataSource()*/);
+        chatAppServer.start();
+        logger.warn("Starting at port {}", chatAppServer.getURL());
     }
 }
