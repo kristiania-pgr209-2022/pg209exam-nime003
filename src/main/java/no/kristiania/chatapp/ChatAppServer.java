adc09ff23340d1819b8fcac98e827f7a5d77d00d
“Mebu98” <merijn.buitelaar98@gmail.com>
2022-11-15 19:38:14 +0100

    🔵ChatAppServer now automatically updates and displays changes from react

--- src/main/java/no/kristiania/chatapp/ChatAppServer.java
+++ src/main/java/no/kristiania/chatapp/ChatAppServer.java
@@ -2,11 +2,14 @@ package no.kristiania.chatapp;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
@@ -15,15 +18,36 @@ public class ChatAppServer {
     private static final Logger logger = LoggerFactory.getLogger(no.kristiania.chatapp.ChatAppServer.class);
     private final Server chatAppServer;
 
-    public ChatAppServer(int port) {
+    public ChatAppServer(int port) throws IOException {
         this.chatAppServer = new Server(port);
 
         var wContext = new WebAppContext();
         wContext.setContextPath("/");
         wContext.setBaseResource(Resource.newClassPathResource("/webapp"));
 
+        Resource resources = Resource.newClassPathResource("/webapp");
+
+        var sourceDirectory = getSourceDirectory(resources);
+
+        if (sourceDirectory != null && sourceDirectory.isDirectory()) {
+            wContext.setBaseResource(Resource.newResource(sourceDirectory));
+            wContext.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        } else {
+            wContext.setBaseResource(resources);
+        }
+
         chatAppServer.setHandler(new HandlerList(wContext));
     }
+
+    private File getSourceDirectory(Resource resources) throws IOException {
+        if(resources.getFile() == null) return null;
+
+        var sourceDir = new File(resources.getFile().getAbsoluteFile().toString()
+                .replace('\\', '/')
+                .replace("target/classes", "src/main/resources"));
+        return sourceDir.exists() ? sourceDir : null;
+    }
+
     public void start() throws Exception {
         chatAppServer.start();
     }
