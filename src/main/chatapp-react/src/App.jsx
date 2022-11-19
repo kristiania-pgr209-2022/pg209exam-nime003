b69309eb43010353b84dd995ef4a97ab6d41029f
nime003 <nime003@student.kristiania.no>
2022-11-19 18:50:20 +0100

    🔵App.jsx display map of users

--- src/main/chatapp-react/src/App.jsx
+++ src/main/chatapp-react/src/App.jsx
@@ -1,27 +1,34 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <ListUsers/>
     </div>
   )
+}
+
+function ListUsers() {
+    const [loading, setLoading] = useState(true);
+    const [username, setUsername] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch("api/user");
+            setUsername(await res.json());
+            setLoading(false);
+        })()
+    })
+
+    if (loading) {
+        return <div>Loading users...</div>
+    }
+    return <ul>
+        <div className={"usergrid"} style={{fontWeight: "bold"}}>Users</div>
+        {username.map(p => <div>{username}</div>)}
+    </ul>
 }
 
 export default App
